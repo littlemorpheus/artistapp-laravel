e3b2fb4a668bab037c399912e1ca56371885bef1
littlemorpheus <Escott97@hotmail.co.uk>
2022-09-01 11:20:33 +0100

    Added routes in, but errors

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/add/{x}/{y}', [NumberController::class, 'add'])->name('numbers.add');
 Route::get('/add', [NumberController::class, 'add2'])->name('numbers.add2');
-
+/*
 Route::get('/elliot', function () {
     // fetch models from the db
     // searching those models - filtering 
@@ -34,10 +34,17 @@ Route::get('/nathan', function () {
 Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
 
 Route::get('/songs/{song}', [SongController::class, 'show'])->name('songs.show');
+*/
 
 //Single Artist View
+Route::get('/artists/{artist}', [SongController::class, 'show'])->name('artists.show');
 //Artist List View
+Route::get('/artists', [SongController::class, 'index'])->name('artists.index');
 //Single Album View
+Route::get('/albums/{album}', [SongController::class, 'show'])->name('albums.show');
 //Album List View
+Route::get('/albums', [SongController::class, 'index'])->name('albums.index');
 //Single Song View
+Route::get('/songs/{song}', [SongController::class, 'show'])->name('songs.show');
 //Song List View
+Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
